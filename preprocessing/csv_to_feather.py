cdf1e3ea53aa77375064d25427413657a78837f7
Lorenzo VILLARD <villard.lorenzo.pro@pm.me>
2020-10-19 15:55:17 +0200

    Implemented data cleaning (with feature engineering).

--- preprocessing/csv_to_feather.py
+++ preprocessing/csv_to_feather.py
@@ -2,7 +2,6 @@ import os
 import gc
 
 import pandas as pd
-from pickle import dump
 
 """
 Convert and merge multiple CSV files into a unique feather file
@@ -22,33 +21,53 @@ fr_columns = [
     "Men_pauv",
     "Ind_snv",
 ]
-us_columns = ["NP", "TAXP", "FES", "YBL", "POVPIP", "WAGP", "NOC", "FPARC"]
+us_columns_housing = ["NP", "TAXP", "FES", "YBL", "NOC", "FPARC", "SERIALNO"]
+us_columns_persons = ["POVPIP", "WAGP", "SERIALNO"]
 
 
 def merged_fr_dataframe():
     fr_dataframes = []
     for file in os.listdir("raw_data/France"):
         fr_dataframes.append(pd.read_csv("raw_data/France/" + file)[fr_columns])
-    return pd.concat(fr_dataframes).reset_index().iloc[:, 1:]
+    return pd.concat(fr_dataframes).reset_index(drop=True)
 
 
 def merged_us_dataframe():
-    us_housing_df_a = pd.read_csv("raw_data/USA/ss15husa.csv")[us_columns]
-    us_housing_df_b = pd.read_csv("raw_data/USA/ss15husa.csv")[us_columns]
-    us_housing_df = pd.concat(us_housing_df_a, us_housing_df_b)
-    us_person_df_a = pd.read_csv("raw_data/USA/ss15pusa.csv")[us_columns]
-    us_person_df_b = pd.read_csv("raw_data/USA/ss15pusb.csv")[us_columns]
-    us_person_df = pd.concat(us_person_df_a, us_person_df_b)
-    return pd.merge(us_housing_df, us_person_df).reset_index().iloc[:, 1:]
+    us_housing_df_a = pd.read_csv(
+        "raw_data/USA/ss15husa.csv", usecols=us_columns_housing
+    )
+    us_housing_df_b = pd.read_csv(
+        "raw_data/USA/ss15husb.csv", usecols=us_columns_housing
+    )
+    us_housing_df = pd.concat([us_housing_df_a, us_housing_df_b])
+    us_person_df_a = pd.read_csv(
+        "raw_data/USA/ss15pusa.csv", usecols=us_columns_persons
+    )
+    us_person_df_b = pd.read_csv(
+        "raw_data/USA/ss15pusb.csv", usecols=us_columns_persons
+    )
+    us_person_df = pd.concat([us_person_df_a, us_person_df_b])
+    return (
+        us_person_df.reset_index(drop=True),
+        us_housing_df.reset_index(drop=True),
+    )
 
 
 def csv_to_feather():
-    merged_fr_dataframe.to_feather("raw_data/fr_data.feather")
+    merged_fr_dataframe().to_feather("raw_data/fr_data.feather")
     gc.collect()  # To avoid overloading memory
-    merged_us_dataframe.to_feather("raw_data/us_data.feather")
-    fr_df = pd.read_feather("raw_data/us_data.feather")
-    us_df = pd.read_feather("raw_data/us_data.feather")
-    assert us_df.shape[1] == len(us_columns)
+    us_person_df, us_housing_df = merged_us_dataframe()
+    us_person_df.to_feather("raw_data/us_data_person.feather")
+    us_housing_df.to_feather("raw_data/us_data_housing.feather")
+    check()
+
+
+def check():
+    fr_df = pd.read_feather("raw_data/fr_data.feather")
+    us_df_p = pd.read_feather("raw_data/us_data_person.feather")
+    us_df_h = pd.read_feather("raw_data/us_data_housing.feather")
+    assert us_df_h.shape[1] == len(us_columns_housing)
+    assert us_df_p.shape[1] == len(us_columns_persons)
     assert fr_df.shape[1] == len(fr_columns)
 
 
---
+++ preprocessing/feature_engineering.py
@@ -0,0 +1,145 @@
+"""
+Some feature engineering :
+-Handle missing values
+-Get or create relevant columns for both datasets
+-Scale input (for a better learning)
+-Split data into train and test sets.
+"""
+
+from pickle import dump
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+import numpy as np
+import pandas as pd
+
+input_features = ["NP", "H1", "H5", "SPH", "HY", "HO"]
+output_features = ["PP", "SL"]
+features = input_features + output_features
+
+us_YBL_converter = {
+    1: 1939,
+    2: 1945,
+    3: 1955,
+    4: 1965,
+    5: 1975,
+    6: 1985,
+    7: 1995,
+    8: 2002,
+    9: 2005,
+    10: 2006,
+    11: 2007,
+    12: 2008,
+    13: 2009,
+    14: 20010,
+    15: 2011,
+    16: 2012,
+    17: 2013,
+}
+
+
+def handle_missing_values(fr_dataframe, us_person_df, us_housing_df):
+    # Drop lines with no prediction
+    us_person_df = us_person_df[us_person_df["POVPIP"].notna()]
+    us_person_df = us_person_df[us_person_df["WAGP"].notna()]
+    fr_dataframe = fr_dataframe[fr_dataframe["Men_pauv"].notna()]
+    fr_dataframe = fr_dataframe[fr_dataframe["Ind_snv"].notna()]
+    # For input data, fill empty values with median
+    fr_imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+    us_imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+    imp_fr_dataframe = pd.DataFrame(fr_imputer.fit_transform(fr_dataframe))
+    imp_fr_dataframe.columns = fr_dataframe.columns
+    imp_fr_dataframe.index = fr_dataframe.index
+    imp_us_housing_df = pd.DataFrame(us_imputer.fit_transform(us_housing_df))
+    imp_us_housing_df.columns = us_housing_df.columns
+    imp_us_housing_df.index = us_housing_df.index
+    return fr_dataframe, us_person_df, us_housing_df
+
+
+def shuffle(dataframe):
+    return dataframe.sample(frac=1).reset_index(drop=True)
+
+
+def split_and_scale(dataframe, directory):
+    dataframe = shuffle(dataframe)
+    X = dataframe[input_features]
+    y = dataframe[output_features]
+    scaler = StandardScaler().fit(X)
+    path = "data/" + directory + "/scaler.pkl"
+    dump(scaler, open(path, "wb"))
+    X = scaler.transform(X)
+    test_proportion = 0.2
+    test_set_size = int(test_proportion * len(dataframe))
+    y_test, y_train = y[:test_set_size], y[test_set_size:]
+    X_test, X_train = X[:test_set_size], X[test_set_size:]
+    X_test = pd.DataFrame(X_test, columns=dataframe[input_features].columns)
+    X_train = pd.DataFrame(X_train, columns=dataframe[input_features].columns)
+    y_test = pd.DataFrame(y_test, columns=dataframe[output_features].columns)
+    y_train = pd.DataFrame(y_train, columns=dataframe[output_features].columns)
+    return X_train, y_train, X_test, y_test
+
+
+def feature_engineering(fr_dataframe, us_person_df, us_housing_df):
+    return (
+        fr_engineering(fr_dataframe),
+        us_engineering(us_person_df, us_housing_df),
+    )
+
+
+def us_engineering(us_person_df, us_housing_df):
+    # Merge person and housing dataframes
+    df_povpip = us_person_df[["SERIALNO", "POVPIP"]].groupby("SERIALNO").max()
+    df_wagp = us_person_df[["SERIALNO", "WAGP"]].groupby("SERIALNO").sum()
+    us_person_df = df_wagp.merge(df_povpip, on="SERIALNO")
+    us_dataframe = us_person_df.join(us_housing_df)
+    # Drop empty houses
+    us_dataframe = us_dataframe[us_dataframe["NP"] >= 1]
+    us_dataframe["YBL"].replace(us_YBL_converter, inplace=True)
+    us_dataframe["HY"] = us_dataframe["YBL"]
+    us_dataframe["H1"] = (us_dataframe["NP"] == 1).astype(int)
+    us_dataframe["H5"] = (us_dataframe["NP"] >= 5).astype(int)
+    us_dataframe["SPH"] = (us_dataframe["NP"] >= 5).astype(int)
+    us_dataframe["HO"] = (us_dataframe["TAXP"] > 1).astype(int)
+    us_dataframe["PP"] = (us_dataframe["POVPIP"] < 100).astype(int)
+    # CU stands for Consumption Units ; used for Standard of living
+    us_dataframe["CU"] = us_dataframe.apply(count_cu, axis=1)
+    us_dataframe["SL"] = us_dataframe["WAGP"] / us_dataframe["CU"]
+    return us_dataframe[features]
+
+
+def count_cu(row):
+    number_of_person = row["NOC"]
+    number_of_children = row["NP"]
+    number_of_person -= 1
+    remaining_adults = number_of_person - number_of_children
+    return 1 + remaining_adults * 0.5 + number_of_children * 0.3
+
+
+def fr_engineering(fr_dataframe):
+    fr_dataframe["NP"] = fr_dataframe["Ind"] / fr_dataframe["Men"]
+    fr_dataframe["H1"] = fr_dataframe["Men_1ind"] / fr_dataframe["Men"]
+    fr_dataframe["H5"] = fr_dataframe["Men_5ind"] / fr_dataframe["Men"]
+    fr_dataframe["SPH"] = fr_dataframe["Men_fmp"] / fr_dataframe["Men"]
+    df_weighted_sum = (
+        fr_dataframe["Log_av45"] * 1945
+        + fr_dataframe["Log_45_70"] * 1958
+        + fr_dataframe["Log_70_90"] * 1980
+        + fr_dataframe["Log_ap90"] * 1995
+    )
+    df_sum = (
+        fr_dataframe["Log_av45"]
+        + fr_dataframe["Log_45_70"]
+        + fr_dataframe["Log_70_90"]
+        + fr_dataframe["Log_ap90"]
+    )
+    fr_dataframe["HY"] = df_weighted_sum / df_sum
+    fr_dataframe["HO"] = (
+        fr_dataframe["Men_prop"].astype("float") / fr_dataframe["Men"]
+    )
+    fr_dataframe["PP"] = (
+        fr_dataframe["Men_pauv"].astype("float") / fr_dataframe["Men"]
+    )
+    fr_dataframe["SL"] = (
+        fr_dataframe["Ind_snv"].astype("float") / fr_dataframe["Ind"]
+    )
+    return fr_dataframe[features]
